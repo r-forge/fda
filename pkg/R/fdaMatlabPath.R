5b092c14149f41d44beba4cc8bff6001e0b33054
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-06-19 07:44:09 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fda@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/fdaMatlabPath.R
@@ -0,0 +1,44 @@
+fdaMatlabPath <- function(startupFile, R.matlab) {
+##
+## 1.  path2fdaM = path to ~R/library/fda/Matlab/fdaM
+##
+  path2fdaM <- system.file('Matlab/fdaM', package='fda')
+##
+## 2.  dirs2add = dirs(path2fdaM, ...)
+##
+  dirs2add <- dirs(path2fdaM, full.names=TRUE, recursive=TRUE)
+##
+## 3.  requires(R.matlab)?
+##
+  missRmat <- missing(R.matlab)
+  if(missRmat)R.matlab <- TRUE 
+  if(R.matlab){
+    if(require(R.matlab))
+      dirs2add <- c(system.file('externals', package='R.matlab'),
+                    dirs2add)
+    else
+      if(!missRmat)
+        warning('Package R.matlab is not installed and can not be',
+                ' included in the Matlab path.')
+  }
+##
+## 4.  Create Matlab 'addpath' commands.
+##
+  d2a <- paste("addpath('", dirs2add, "');", sep='')
+##
+## 5.  write file
+##
+  writeLines(d2a, 'fdaMatlabPath.m')
+##
+## 6.  startupFile?  
+##
+  if(!missing(startupFile)){
+    if(exists(startupFile))
+      file.copy2(startupFile)
+    cat(d2a, file=startupFile, sep='\n', append=TRUE)
+  }
+##
+## 7.  Done
+##
+  d2a  
+}
