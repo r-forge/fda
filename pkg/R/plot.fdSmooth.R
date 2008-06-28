58dbedc82a57a3f13a4cb1d2aa2f6d7a5ca0f33f
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-06-28 18:12:42 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fda@47 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.fdSmooth.R
+++
@@ -1,8 +0,0 @@
-plot.fdSmooth <- function(x, y, Lfdobj=0, href=TRUE, titles=NULL,
-                          xlim=rangex, ylim=rangey, xlab=xlabel,
-                          ylab=ylabel, ask=FALSE, nx=201, ...){
-  plot(x$fd, y, Lfdobj=0, href=TRUE, titles=NULL,
-              xlim=xlim, ylim=ylim, xlab=xlab,
-              ylab=ylab, ask=FALSE, nx=201, ...)
-}
-  
