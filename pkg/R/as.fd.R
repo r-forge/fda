4ecb03ff4e30ce57adc64f3c7ec8e44ad1ee0e3b
spencerg (FinTS ) <spencer.graves@prodsyse.com>
2008-08-12 03:01:08 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fda@87 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/as.fd.R
+++ pkg/R/as.fd.R
@@ -2,6 +2,10 @@ as.fd <- function(x, ...){
   UseMethod('as.fd')
 }
 
+as.fd.fdSmooth <- function(x, ...){
+  x$fd
+}
+
 as.fd.dierckx <- function(x, ...){ 
 # Translate an object of class dierckx to class fd
 ##
@@ -79,20 +83,18 @@ as.fd.function <- function(x, ...){
     stop("'x' (", objName, ") has a NULL 'method', and therefore",
          " can NOT have been created by 'splinefun.'")
 # z$method:  1=periodic, 2=natural, 3=fmm (std B-Splines, I believe)   
-  if(xz$method!=3){
+#  if(xz$method!=3){
+  if(!(xz$method %in% 2:3)){
     msg <- paste("x (", objName, ") ", sep='')
-    msg2 <- switch(xz$method,
-                 "1" = paste(msg, " uses periodic B-splines, ",
-                   "and as.fd is programmed to translate only ",
-                   "B-splines with coincident boundary knots.",
-                   sep=''),
-                 '2' = paste(msg, "uses natural splines.  ",
-                   "and as.fd is programmed to translate only ",
-                   "B-splines with coincident boundary knots.",
-                   sep=''),
-                paste(msg, "does not use B-splines as required ",
+    msg2 <- {
+      if(xz$method=="1")
+        paste(msg, " uses periodic B-splines, and as.fd ",
+              "is programmed\n    to translate only B-splines ",
+              "with coincident boundary knots.", sep='')
+      else
+        paste(msg, "does not use B-splines as required ",
                       "for function 'as.fd'.")
-                 )
+    }
     stop(msg2)
   }
 ##
@@ -108,3 +110,33 @@ as.fd.function <- function(x, ...){
   x. <- seq(Knots[1], Knots[nKn], length=nobs) 
   smooth.basis(x., x(x.), fd.)$fd
 }
+
+as.fd.smooth.spline <- function(x, ...){
+# Translate an object of class smooth.spline to class fd
+##
+## 1.  check class
+##
+  objName <- deparse(substitute(x))
+  {
+    if(length(objName)>1)
+      objName <- character(0)
+    else
+      if(nchar(objName)>33)
+        objName <- substring(objName, 1, 33)
+  }
+  if(!inherits(x, 'smooth.spline')) 
+    stop("'x' (", objName, ") is not of class smooth.spline")
+##
+## 2.  Create a basis 
+##
+  Kn0 <- x$fit$knot
+  x0 <- min(x$x)
+  x1 <- max(x$x) 
+  Knots <- (x0+(x1-x0)*Kn0[4:(length(Kn0)-3)])
+# Don't use 'unique' in case 'x' has coincident interior knots.
+#  basis <- create.bspline.basis(breaks=Knots)
+  basis <- create.bspline.basis(range(Knots), breaks=Knots)
+#
+  fd(x$fit$coef, basis)
+}
+
