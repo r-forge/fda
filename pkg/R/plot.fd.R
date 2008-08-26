4072b3989e41446556c6a9caaa2b3e9ba5da8050
ramsay (Prof ) <ramsay@psych.mcgill.ca>
2008-08-26 21:05:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fda@101 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.fd.R
+++ pkg/R/plot.fd.R
@@ -1,27 +1,20 @@
-plot.fdSmooth <- function(x, y, Lfdobj=0, href=TRUE, titles=NULL,
-                          xlim=NULL, ylim=NULL, xlab=NULL,
-                          ylab=NULL, ask=FALSE, nx=201, ...){
-  plot(x$fd, y, Lfdobj=Lfdobj, href=href, titles=titles,
-              xlim=xlim, ylim=ylim, xlab=xlab,
-              ylab=ylab, ask=ask, nx=nx, ...)
-}
-
-plot.fd <- function(x, y, Lfdobj=0, href=TRUE, titles=NULL,
-                    xlim=NULL, ylim=NULL, xlab=NULL,
-                    ylab=NULL, ask=FALSE, nx=201, ...)
+plot.fd <- function(x, y, Lfdobj=0, href=TRUE, 
+                    xlim=rangex, ylim=rangey, 
+                    xlab=xlabel, ylab=ylabel, 
+                    ask=FALSE,   nx=nxdefault, ...)
 {
 #  -----------------------------------------------------------------------
 #       plot for fd class
 #  -----------------------------------------------------------------------
-#  Plot a functional data object fdobj.
-#  Arguments:
-#  x = fdobj     ... a functional data object
-#  Lfdobj    ... linear differental operator to be applied to fdobj before
-#             plotting
-#  HREF   ... If TRUE, a horizontal dotted line through 0 is plotted.
-# ...   
-#  ASK, if TRUE, causes the curves to be displayed one at a time.
-#  NX     ... The number of sampling points to use for
+
+  #  Plot a functional data object fdobj.
+  #  Arguments:
+  #  fdobj     ... a functional data object
+  #  Lfdobj    ... linear differental operator to be applied to fdobj before
+  #             plotting
+  #  HREF   ... If TRUE, a horizontal dotted line through 0 is plotted.
+  #  The argument ASK, if TRUE, causes the curves to be displayed one at a time.
+  #  NX     ... The number of sampling points to use for
   #             plotting.  (default 101)
   
   #  The remaining optional arguments are the same as those available
@@ -31,12 +24,8 @@ plot.fd <- function(x, y, Lfdobj=0, href=TRUE, titles=NULL,
   #    must be set up before calling plot by using something such as
   #    par(mfrow=c(1,nvar),pty="s")
 
-  # last modified 2008.06.28 by Spencer Graves 
-  #  previously modified 2007.05.03 and 6 March 2007
-
-  # fdobj, Lfdobj=0, href=TRUE, nx=201, titles=NULL,
-  #                   xlab=xlabel, ylab=ylabel,
-  #                   xlim=rangex, ylim=rangey, ask=FALSE
+  # last modified 25 August 2008 by Jim Ramsay
+  # previously modified 3 May 2007 by Spencer Graves
 
   #  check fdobj
 
@@ -46,25 +35,19 @@ plot.fd <- function(x, y, Lfdobj=0, href=TRUE, titles=NULL,
 
   #  check Lfdobj
 
-#  if (missing(Lfdobj)) Lfdobj <- 0
   Lfdobj <- int2Lfd(Lfdobj)
   if (!inherits(Lfdobj, "Lfd")) stop(
       "Second argument is not a linear differential operator.")
-
-  #  check href
-# if (missing(href)) href <- TRUE
-
-  #  check ask
-# if (missing(ask)) ask <- FALSE
 
   #  extract dimension information
 
   coef   <- fdobj$coefs
   coefd  <- dim(coef)
   ndim   <- length(coefd)
-# Number of basis functions   
-  nbasis <- coefd[1]
-# Number of functional observations   
+  # Number of basis functions   
+  nbasis    <- coefd[1]
+  nxdefault <- 10*nbasis + 1
+  # Number of functional observations   
   nrep   <- coefd[2]
   if (ndim > 2) nvar <- coefd[3] else nvar <- 1
 
@@ -72,14 +55,10 @@ plot.fd <- function(x, y, Lfdobj=0, href=TRUE, titles=NULL,
 
   basisobj <- fdobj$basis
   rangex   <- basisobj$rangeval
-  if(is.null(xlim))xlim <- rangex 
-  #  check xlim
-# if (missing(xlim)) xlim <- rangex
 
   #  set up a set of argument values for the plot
 
   if (missing(y)) {
-#    y <- 201
     y <- nx
   } else {
     y <- as.vector(y)
@@ -93,110 +72,86 @@ plot.fd <- function(x, y, Lfdobj=0, href=TRUE, titles=NULL,
     }
   }
   if (min(y) < rangex[1] || max(y) > rangex[2]) stop(
-    "Values in Y are outside the basis range.")
+    "Values in Y are out of the basis range.")
 
   #  evaluate LFDOBJ(FDOBJ) at the argument values
 
   fdmat    <- eval.fd(y, fdobj, Lfdobj)
   rangey   <- range(c(fdmat))
-  if(is.null(ylim))ylim <- rangey
-  #  check ylim
-# if (missing(ylim)) ylim <- rangey
 
-  xlabel   <- names(fdobj$fdnames)[[1]]
-  ylabel   <- names(fdobj$fdnames)[[3]]
-  if (is.character(xlabel) == FALSE) xlabel <- ""
-  if (is.character(ylabel) == FALSE) ylabel <- ""
+  #  set up axis labels and, 
+  #  optionally, caselabels and variable labels
 
-  #  check xlab and ylab
-  if(is.null(xlab))xlab <- xlabel
-  if(is.null(ylab))ylab <- ylabel
-#  if (missing(xlab)) xlab <- xlabel
-#  if (missing(ylab)) ylab <- ylabel
-  crvnames <- fdobj$fdnames[[2]]
-  varnames <- fdobj$fdnames[[3]]
-# Don't ask for the first plot, but do for later plots if(ask)   
-#  op <- par(ask=FALSE)
-# Don't ask for the first plot,
-# but if ask==TRUE, do ask for succeeding plots 
-#  on.exit(par(op))
-# A single line?  
+  fdnames      = fdobj$fdnames
+  fdlabelslist = fdlabels(fdnames, nrep, nvar)
+
+  xlabel    = fdlabelslist$xlabel
+  ylabel    = fdlabelslist$ylabel
+  casenames = fdlabelslist$casenames
+  varnames  = fdlabelslist$varnames
+
+  # A single line?  
   if (ndim < 2) {
     plot (y, fdmat, type="l", xlim=xlim, ylim=ylim,
           xlab=xlab, ylab=ylab, ...)
-#    if (zerofind(fdmat) && href) abline(h=0,lty=2)
-    if (zerofind(ylim) && href) abline(h=0,lty=2)
+    if (zerofind(fdmat) && href) abline(h=0,lty=2)
   }
-# Several copies of one function?    
+  # Several copies of one function?    
   if (ndim ==2 ) {
     if (!ask) {
-      matplot(y, fdmat, type="l", xlim=xlim, ylim=ylim,
-           		xlab=xlab, ylab=ylab, ...)
-#      if (zerofind(fdmat) && href) abline(h=0,lty=2)
-      if (zerofind(ylim) && href) abline(h=0,lty=2)
+      matplot(y, fdmat, type="l", 
+              xlim=xlim,   ylim=ylim,
+              xlab=xlabel, ylab=ylabel, ...)
+      if (zerofind(fdmat) && href) abline(h=0,lty=2)
     } else  {
-      op <- par(ask=FALSE)
-# Don't ask for the first plot,
-# but if ask==TRUE, do ask for succeeding plots 
-      on.exit(par(op))
-      cat('Multiple plots:  Click in the plot to advance to the next') 
-#      op <- par(ask = TRUE)
-#      on.exit(par(op))
       for (irep in 1:nrep) {
-        plot (y, fdmat[,irep], type="l", xlim=xlim, ylim=ylim,
-                xlab=xlab, ylab=ylab, ...)
-        if(irep<2) par(ask=ask)
-#        if (zerofind(fdmat[,irep]) && href) abline(h=0,lty=2)
-        if (zerofind(ylim) && href) abline(h=0,lty=2)
-        if (!is.null(titles)) title(titles[irep])
-        else title(paste(crvnames[irep]))
-#        else title(paste("Curve",irep,crvnames[irep]), line=0.2)
-#       ... "line=0.2" to allow "main" in "..."         
-#        mtext("Click in graph to see next plot", side=3, outer=FALSE)
-#        text("",locator(1))
+        plot (y, fdmat[,irep], type="l", 
+              xlim=xlim, ylim=ylim,
+              xlab=xlab, ylab=ylab, ...)
+        if (!is.null(casenames)) title(casenames[irep])
+        else                     title(paste("Case",irep))
+        if (zerofind(fdmat[,irep]) && href) abline(h=0,lty=2)
+        mtext("Click in graph to see next plot", side=3, outer=FALSE)
+        text("",locator(1))
       }
     }
   }
-# Possibly multiple copies of different functions   
+  # Possibly multiple copies of different functions   
   if (ndim == 3) {
     if (!ask) {
       for (ivar in 1:nvar) {
-        matplot (y, fdmat[,,ivar], type="l", xlim=xlim, ylim=ylim,
-                 xlab=xlab, ylab=ylab,
-                 main=varnames[ivar], ask=FALSE, ...)
-#        if (zerofind(fdmat[,,ivar]) && href) abline(h=0,lty=2)
-        if (zerofind(ylim) && href) abline(h=0,lty=2)
+        matplot (y, fdmat[,,ivar], type="l", 
+                 xlim=xlim, ylim=ylim,
+                 xlab=xlab, ylab=ylab, ask=FALSE, ...)
+        if (!is.null(varnames)) title(varnames[ivar])
+        else                    title(paste("Variable",ivar))
+        if (zerofind(fdmat[,,ivar]) && href) abline(h=0,lty=2)
       }
     } else {
-      op <- par(ask=FALSE)
-# Don't ask for the first plot,
-# but if ask==TRUE, do ask for succeeding plots 
-      on.exit(par(op))
-      cat('Multiple plots:  Click in the plot to advance to the next') 
       for (irep in 1:nrep) {
-#            if (ivar==1){op <- par(ask = TRUE); on.exit(par(op)) }
-#            else { op <- par(ask = FALSE); on.exit(par(op)) }
         for (ivar in 1:nvar) {
-          plot(y,fdmat[,irep,ivar],type="l", xlim=xlim, ylim=ylim,
-                xlab=xlab, ylab=ylab, ...)
-          if((irep<2) && (ivar<2)) par(ask=ask)
-#          if (zerofind(fdmat[,irep,ivar]) && href) abline(h=0,lty=2)
-          if (zerofind(ylim) && href) abline(h=0,lty=2)
-          if (!is.null(titles)) title(titles[irep])
-          else title(paste("Curve", irep, varnames[ivar]))
-#          mtext("Click in graph to see next plot", side=3, outer=FALSE)
-#          text("",locator(1))
+          plot(y,fdmat[,irep,ivar],type="l", 
+               xlim=xlim, ylim=ylim,
+               xlab=xlab, ylab=ylab, ...)
+          if (!is.null(casenames)) titlestr = casenames[irep]
+          else                     titlestr = paste("Case",irep)
+          if (!is.null(varnames)) { 
+             titlestr = paste(titlestr,"  ",varnames[ivar])
+          } else {                   
+             titlestr = paste(titlestr,"  ","Variable",ivar)
+          }
+          title(titlestr)
+          if (zerofind(fdmat[,irep,ivar]) && href) abline(h=0,lty=2)
+          mtext("Click in graph to see next plot", side=3, outer=FALSE)
+          text("",locator(1))
         }
       }
     }
   }
-#  invisible(NULL)
-# This used to return 'invisible(NULL)'.
-# However, with R 2.7.0 under XEmacs with ESS,
-# it sometimes failed to plot.  I changed the return value,
-# and the problem disappeared.  
-  'done'
+  invisible(NULL)
 }
+
+#  --------------------------------------------------------------------
 
 zerofind <- function(fmat)
 {
@@ -204,3 +159,6 @@ zerofind <- function(fmat)
   if (frng[1] <= 0 && frng[2] >= 0) zeroin <- TRUE else zeroin <- FALSE
   return(zeroin)
 }
+
+
+
