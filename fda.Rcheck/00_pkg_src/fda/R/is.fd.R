8ee2979e9a3facb2c6aa7fc59413db8f9b96f3ed
gilesh (Prof ) <gjh27@cornell.edu>
2008-09-30 23:13:16 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fda@109 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- fda.Rcheck/00_pkg_src/fda/R/is.fd.R
+++
@@ -1,7 +0,0 @@
-is.fd <- function(fdobj) {
-#  check whether FDOBJ is a functional data object
-
-#  Last modified 20 November 2005
-
-  if (inherits(fdobj, "fd")) return(TRUE) else return(FALSE)
-}
--- fda.Rcheck/00_pkg_src/fda/R/is.fdPar.R
+++
@@ -1,7 +0,0 @@
-is.fdPar <- function(fdParobj) {
-#  check whether FDPAROBJ is a functional data object
-
-#  Last modified 20 November 2005
-
-  if (inherits(fdParobj, "fdPar")) return(TRUE) else return(FALSE)
-}
