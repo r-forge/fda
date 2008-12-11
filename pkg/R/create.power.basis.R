005738010719d0c1773e3d7463088b4a34af8bb9
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-11 16:05:21 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fda@236 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/create.power.basis.R
+++ pkg/R/create.power.basis.R
@@ -1,7 +1,8 @@
 create.power.basis <- function(rangeval  = c(0,1), nbasis = NULL,
                                exponents = NULL, dropind=NULL,
                                quadvals=NULL, values=NULL,
-                               basisvalues=NULL, names='power')
+                               basisvalues=NULL, names='power',
+                               axes=NULL)
 {
 #  This function creates an power functional data basis
 #  Arguments
@@ -204,7 +205,10 @@ create.power.basis <- function(rangeval  = c(0,1), nbasis = NULL,
       paste(names, 0:(nbasis-1), sep="")
     }
   }
-
+##
+## 6.  Done
+##
+  if(!is.null(axes))basisobj$axes <- axes
   basisobj
 
 }
