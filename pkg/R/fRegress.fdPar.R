a5521e38664c1ea3340d3d0e1cfde4a9aaf6587e
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-25 00:16:06 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fda@270 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/fRegress.fdPar.R
+++ pkg/R/fRegress.fdPar.R
@@ -60,10 +60,12 @@ fRegress.fdPar <- function(y, xfdlist, betalist, wt=NULL,
 #    wt          ... weights for observations
 #    df          ... degrees of freedom for fit
 
-# Last modified 2008.12.19 by Spencer
-#  Last modified 11 December 2008 by Jim
+# Last modified 2008.12.24 by Spencer
+# Previously modified 11 December 2008 by Jim
 
-#  check YFDPAR and compute sample size N
+##
+## 1.  check YFDPAR and compute sample size N
+##
   yfdPar <- y
   if (inherits(yfdPar, "fd")) yfdPar <- fdPar(yfdPar)
 
@@ -79,115 +81,115 @@ fRegress.fdPar <- function(y, xfdlist, betalist, wt=NULL,
            'dim(yfdPar$fd$coefs) = ', paste(ydim, collapse=', '))
     N     <- ydim[2]
   }
-
-#  get number of independent variables p
-
-p <- length(xfdlist)
+##
+## 2.  get number of independent variables p & check betalist
+##
+  p <- length(xfdlist)
 
 #  check BETALIST
 
-if (inherits(betalist, "fd")) betalist <- list(betalist)
+  if (inherits(betalist, "fd")) betalist <- list(betalist)
 
-if (!inherits(betalist, "list")) stop(
-	"Argument BETALIST is not a list object.")
+  if (!inherits(betalist, "list"))
+    stop("Argument BETALIST is not a list object.")
 
-if (length(betalist) != p)  {
-	cat(paste("\nNumber of regression coefficients does not match\n",
-		       "number of independent variables."))
-	stop("")
-}
+  if (length(betalist) != p)  {
+    cat(paste("\nNumber of regression coefficients does not match\n",
+              "number of independent variables."))
+    stop("")
+  }
 
-berror <- FALSE
-for (j in 1:p) {
-	betafdParj <- betalist[[j]]
-	if (inherits(betafdParj, "fd") || inherits(betafdParj, "basisfd")) {
-		betafdParj    <- fdPar(betafdParj)
-		betalist[[j]] <- betafdParj
-	}
-	if (!inherits(betafdParj, "fdPar")) {
-		print(paste("BETALIST[[",j,"]] is not a FDPAR object."))
-		berror <- TRUE
-	}
-}
+  berror <- FALSE
+  for (j in 1:p) {
+    betafdParj <- betalist[[j]]
+    if (inherits(betafdParj, "fd") || inherits(betafdParj, "basisfd")) {
+      betafdParj    <- fdPar(betafdParj)
+      betalist[[j]] <- betafdParj
+    }
+    if (!inherits(betafdParj, "fdPar")) {
+      print(paste("BETALIST[[",j,"]] is not a FDPAR object."))
+      berror <- TRUE
+    }
+  }
 
-if (berror) stop("")
+  if (berror) stop("")
 
-betafd1    <- betalist[[1]]$fd
-betabasis1 <- betafd1$basis
-rangeval   <- betabasis1$rangeval
-
-#  check XFDLIST
-
-if (inherits(xfdlist, "fd") || inherits(xfdlist, "numeric"))
+  betafd1    <- betalist[[1]]$fd
+  betabasis1 <- betafd1$basis
+  rangeval   <- betabasis1$rangeval
+##
+## 3.  check XFDLIST
+##
+  if (inherits(xfdlist, "fd") || inherits(xfdlist, "numeric"))
     xfdlist <- list(xfdlist)
 
-if (!inherits(xfdlist, "list")) stop(
-	"Argument XFDLIST is not a list object.")
+  if (!inherits(xfdlist, "list"))
+    stop("Argument XFDLIST is not a list object.")
 
 #  check each xfdlist member.  If the object is a vector of length N,
 #  it is converted to a functional data object with a
 #  constant basis
 
-onebasis <- create.constant.basis(rangeval)
-onesfd   <- fd(1,onebasis)
+  onebasis <- create.constant.basis(rangeval)
+  onesfd   <- fd(1,onebasis)
 
-xerror <- FALSE
-for (j in 1:p) {
+  xerror <- FALSE
+  for (j in 1:p) {
     xfdj <- xfdlist[[j]]
     if (inherits(xfdj, "fd")) {
-        xcoef <- xfdj$coefs
-        if (length(dim(xcoef)) > 2) stop(
-            paste("Covariate",j,"is not univariate."))
-        #  check size of coefficient array
-        Nj    <- dim(xcoef)[2]
-        if (Nj != N) {
-            print(
-               paste("Incorrect number of replications in XFDLIST",
+      xcoef <- xfdj$coefs
+      if (length(dim(xcoef)) > 2)
+        stop(paste("Covariate",j,"is not univariate."))
+#  check size of coefficient array
+      Nj    <- dim(xcoef)[2]
+      if (Nj != N) {
+        print(paste("Incorrect number of replications in XFDLIST",
                      "for covariate",j))
-            xerror = TRUE
-        }
+        xerror = TRUE
+      }
     }
-    if (inherits(xfdj, "numeric")) {
-        if (!is.matrix(xfdj)) xfdj = as.matrix(xfdj)
-	  Zdimj <- dim(xfdj)
-        if (Zdimj[1] != N) {
-            print(paste("Vector in XFDLIST[[",j,"]] has wrong length."))
-				    xerror = TRUE
-		    }
-        if (Zdimj[2] != 1) {
-            print(paste("Matrix in XFDLIST[[",j,"]] has more than one column."))
-				    xerror = TRUE
-		    }
-        xfdlist[[j]] <- fd(matrix(xfdj,1,N), onebasis)
+    if (is.numeric(xfdj)) {
+      if (!is.matrix(xfdj)) xfdj = as.matrix(xfdj)
+      Zdimj <- dim(xfdj)
+      if (Zdimj[1] != N) {
+        print(paste("Vector in XFDLIST[[",j,"]] has wrong length."))
+        xerror = TRUE
+      }
+      if (Zdimj[2] != 1) {
+        print(paste("Matrix in XFDLIST[[",j,
+                    "]] has more than one column."))
+        xerror = TRUE
+      }
+      xfdlist[[j]] <- fd(matrix(xfdj,1,N), onebasis)
     }
     if (!(inherits(xfdlist[[j]], "fd") ||
-          inherits(xfdlist[[j]], "numeric"))) {
+          is.numeric(xfdlist[[j]]))) {
       print(paste("XFDLIST[[",j,"]] is neither an FD object nor numeric."))
       xerror = TRUE
     }
-}
+  }
 
-if (xerror) stop("")
+  if (xerror) stop("problem with xfdlist")
 
 #  check weights
 
-{
+  {
     if(is.null(wt)){
-        wt <- rep(1, N)
-        wtconstant <- TRUE
+      wt <- rep(1, N)
+      wtconstant <- TRUE
     }
     else {
-        if (length(wt) != N) stop("Number of weights not equal to N.")
-        if (any(wt < 0))     stop("Negative weights found.")
-        wtconstant <- (var(wt) == 0)
+      if (length(wt) != N) stop("Number of weights not equal to N.")
+      if (any(wt < 0))     stop("Negative weights found.")
+      wtconstant <- (var(wt) == 0)
     }
-}
+  }
 
 #  --------------------------------------------------------------------------
 #  branch depending on whether the dependent variable is functional or scalar
 #  --------------------------------------------------------------------------
 
-if (inherits(yfdPar, "fdPar")) {
+  if (inherits(yfdPar, "fdPar")) {
 
     #  ----------------------------------------------------------------
     #           YFDPAR is a functional parameter object
